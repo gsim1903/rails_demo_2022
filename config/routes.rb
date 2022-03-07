94858ac995bc54f6b15cba831c49e62af9aba375
gsim1903 <gavin@internetbusker.co>
2022-03-07 11:15:56 +0100

    tidy up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :pings, only: [:index]
+    # resources :pings, only: [:index]
+    get "pings", controller: :pings, action: :index
   end
 end
