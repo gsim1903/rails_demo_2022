c3800838407e48d7edc6eff514e44cd6ec3200fa
gsim1903 <gavin@internetbusker.co>
2022-03-07 12:10:21 +0100

    refactor test code

--- spec/requests/articles/index_spec.rb
+++ spec/requests/articles/index_spec.rb
@@ -7,9 +7,7 @@ RSpec.describe 'GET /api/articles', type: :request do
 
   subject { response }
 
-  it 'is expected to respond with status code 200' do
-    expect(subject.status).to eq 200
-  end
+  it {is_expected.to have_http_status 200}
 
   it ' is expectd to return a collection of 5 articles' do
     expect(response_json['articles'].size).to eq 5
