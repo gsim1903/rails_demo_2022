a6a3160eed639124439c06528c8b4e59e2dfdf27
gsim1903 <gavin@internetbusker.co>
2022-03-09 06:31:32 +0100

    refactors create method and spec code

--- spec/requests/articles/create_spec.rb
+++ spec/requests/articles/create_spec.rb
@@ -6,24 +6,17 @@ RSpec.describe 'POST /api/articles' do
         body: 'Lorem ipsum...'
       }
     }
+    @article = Article.last 
   end
 
   subject { response }
 
   it { is_expected.to have_http_status 201 }
 
-  it 'is expected to create an instance of an article' do
-    expect(Article.last).not_to be nil
+  it 'is expected to create an instance of an article with a title and body' do
+    expect(@article).not_to be nil
+    expect(@article.title).to eq "News about coding"
+    expect(@article.body).to eq "Lorem ipsum..."
   end
-
-  it "is expected to have a title" do
-    expect(Article.last.title).to eq "News about coding"
-end
-
-it "is expected to have a title" do
-    expect(Article.last.body).to eq "Lorem ipsum..."
-end
-
-
 
 end
